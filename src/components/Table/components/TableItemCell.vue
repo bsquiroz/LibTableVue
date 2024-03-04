b8465ad2e7cdbc5d7f61681659da00f4ae3bfe7d
bsquiroz <bemq1605@gmail.com>
2024-03-03 21:46:04 -0500

    refactor nameFilesCells

--- src/components/Table/components/TableItemCell.vue
+++ src/components/Table/components/TableItemCell.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { ValueColumnTable } from "../interface";
 
-import CellCheck from "./TableBodyCell/CellBodyCheck.vue";
-import CellDate from "./TableBodyCell/CellBodyDate.vue";
-import CellDefault from "./TableBodyCell/CellBodyDefault.vue";
-import CellEdit from "./TableBodyCell/CellBodyEdit.vue";
-import CellSetting from "./TableBodyCell/CellBodySetting.vue";
-import CellStatus from "./TableBodyCell/CellBodyStatus.vue";
-import CellType from "./TableBodyCell/CellBodyType.vue";
-import CellUrl from "./TableBodyCell/CellBodyUrl.vue";
+import CellBodyCheck from "./TableBodyCell/CellBodyCheck.vue";
+import CellBodyDate from "./TableBodyCell/CellBodyDate.vue";
+import CellBodyDefault from "./TableBodyCell/CellBodyDefault.vue";
+import CellBodyEdit from "./TableBodyCell/CellBodyEdit.vue";
+import CellBodySetting from "./TableBodyCell/CellBodySetting.vue";
+import CellBodyStatus from "./TableBodyCell/CellBodyStatus.vue";
+import CellBodyType from "./TableBodyCell/CellBodyType.vue";
+import CellBodyUrl from "./TableBodyCell/CellBodyUrl.vue";
 
 interface Cell {
     field: ValueColumnTable;
@@ -19,12 +19,12 @@ defineProps<Cell>();
 </script>
 
 <template>
-    <CellCheck v-if="field === 'check'" />
-    <CellType v-else-if="field === 'pill'" :type="value" />
-    <CellStatus v-else-if="field === 'switch'" />
-    <CellEdit v-else-if="field === 'edit'" />
-    <CellSetting v-else-if="field === 'setting'" />
-    <CellDate v-else-if="field === 'date'" :time="value" />
-    <CellUrl v-else-if="field === 'url'" :url="value" />
-    <CellDefault v-else :value="value" />
+    <CellBodyCheck v-if="field === 'check'" />
+    <CellBodyType v-else-if="field === 'pill'" :type="value" />
+    <CellBodyStatus v-else-if="field === 'switch'" />
+    <CellBodyEdit v-else-if="field === 'edit'" />
+    <CellBodySetting v-else-if="field === 'setting'" />
+    <CellBodyDate v-else-if="field === 'date'" :time="value" />
+    <CellBodyUrl v-else-if="field === 'url'" :url="value" />
+    <CellBodyDefault v-else :value="value" />
 </template>
