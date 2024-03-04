365ac8cde541e67be95181d4a97eab2d0a862b0f
bsquiroz <bemq1605@gmail.com>
2024-03-04 15:10:12 -0500

    extract component setting

--- src/components/Table/components/TableBodyCell/CellBodySetting.vue
+++ src/components/Table/components/TableBodyCell/CellBodySetting.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
+import TableItemActions from "../TableItemActions.vue";
+
 const isShowMenuSetting = ref(false);
 </script>
 
@@ -17,52 +19,7 @@ const isShowMenuSetting = ref(false);
                 {{ isShowMenuSetting ? "close" : "more_vert" }}
             </span>
 
-            <div
-                class="absolute top-[100%] z-50 right-0 bg-white p-2 rounded-lg flex flex-col transition-all"
-                :class="{
-                    'opacity-100 visible': isShowMenuSetting,
-                    'opacity-0 invisible': !isShowMenuSetting,
-                }"
-            >
-                <article class="box__item__setting">
-                    <span class="material-symbols-outlined text-sm">
-                        edit_square
-                    </span>
-                    <span>Edit</span>
-                </article>
-                <article class="box__item__setting">
-                    <span class="material-symbols-outlined text-sm">
-                        content_copy
-                    </span>
-                    <span>Copy</span>
-                </article>
-                <article class="box__item__setting">
-                    <span class="material-symbols-outlined text-sm">
-                        domain_verification_off
-                    </span>
-                    <span>Unpublish</span>
-                </article>
-                <article class="box__item__setting">
-                    <span class="material-symbols-outlined text-sm">
-                        visibility
-                    </span>
-                    <span>Preview</span>
-                </article>
-                <article class="box__item__setting">
-                    <span
-                        class="material-symbols-outlined text-sm text-red-500"
-                    >
-                        delete
-                    </span>
-                    <span class="text-red-500">Delete</span>
-                </article>
-            </div>
+            <TableItemActions :isShowMenuSetting="isShowMenuSetting" />
         </div>
     </td>
 </template>
-
-<style scoped>
-.box__item__setting {
-    @apply flex gap-2 cursor-pointer hover:opacity-80 p-2;
-}
-</style>
---
+++ src/components/Table/components/TableItemActions.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+interface Props {
+    isShowMenuSetting: boolean;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+    <div
+        class="absolute top-[100%] z-50 right-0 bg-white p-2 rounded-lg flex flex-col justify-start items-start transition-all"
+        :class="{
+            'opacity-100 visible': isShowMenuSetting,
+            'opacity-0 invisible': !isShowMenuSetting,
+        }"
+    >
+        <article class="box__item__setting">
+            <span class="material-symbols-outlined text-sm"> edit_square </span>
+            <span>Edit</span>
+        </article>
+        <article class="box__item__setting">
+            <span class="material-symbols-outlined text-sm">
+                content_copy
+            </span>
+            <span>Copy</span>
+        </article>
+        <article class="box__item__setting">
+            <span class="material-symbols-outlined text-sm">
+                domain_verification_off
+            </span>
+            <span>Unpublish</span>
+        </article>
+        <article class="box__item__setting">
+            <span class="material-symbols-outlined text-sm"> visibility </span>
+            <span>Preview</span>
+        </article>
+        <article class="box__item__setting">
+            <span class="material-symbols-outlined text-sm text-red-500">
+                delete
+            </span>
+            <span class="text-red-500">Delete</span>
+        </article>
+    </div>
+</template>
+
+<style scoped>
+.box__item__setting {
+    @apply flex gap-2 cursor-pointer hover:opacity-80 p-2;
+}
+</style>
